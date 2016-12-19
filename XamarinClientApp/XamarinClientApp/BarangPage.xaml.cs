b9492bb89191edbfc1ac46ab18d6ccb82ad21672
HIDAYAT <HIDAYAT@HIDAYAT-PC>
2016-12-19 14:41:29 +0700

    Add project files.

---
+++ XamarinClientApp/XamarinClientApp/BarangPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using XamarinClientApp.Model;
+using XamarinClientApp.ViewModel;
+using RestSharp.Portable;
+
+namespace XamarinClientApp
+{
+    public partial class BarangPage : ContentPage
+    {
+        public BarangPage()
+        {
+            InitializeComponent();
+            listBarang.ItemTapped += ListBarang_ItemTapped;
+            btnTambah.Clicked += BtnTambah_Clicked;
+           
+        }
+
+        protected override void OnAppearing()
+        {
+            this.BindingContext = new BarangViewModel();
+        }
+
+        private void ListBarang_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Barangs item = (Barangs)e.Item;
+            EditBarangPage editPage = new EditBarangPage();
+            editPage.BindingContext = item;
+            Navigation.PushAsync(editPage);
+        }
+
+        private async void BtnTambah_Clicked(object sender, EventArgs e)
+        {
+            TambahBarangPage tambahPage = new TambahBarangPage();
+            Navigation.PushAsync(tambahPage);
+        }
+
+       
+    } }
---
+++ XamarinClientApp/XamarinClientApp/ViewModel/BarangViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+using XamarinClientApp.Model;
+using RestSharp;
+using RestSharp.Portable.HttpClient;
+using RestSharp.Portable;
+
+namespace XamarinClientApp.ViewModel
+{
+    public class BarangViewModel : BindableObject
+    {
+        private RestClient _client =
+           new RestClient("http://onderdilshop.azurewebsites.net/");
+
+        private ObservableCollection<Barangs> listBarang;
+        public ObservableCollection<Barangs> ListBarang
+        {
+            get { return listBarang; }
+            set { listBarang = value; OnPropertyChanged("ListBarang"); }
+        }
+
+        private async void RefreshDataAsync()
+        {
+            RestRequest _request = new RestRequest("api/Barangs", Method.GET);
+            var _response = await _client.Execute<List<Barangs>>(_request);
+            ListBarang = new ObservableCollection<Barangs>(_response.Data);
+        }
+
+        public BarangViewModel()
+        {
+            RefreshDataAsync();
+        }
+    }
+}
